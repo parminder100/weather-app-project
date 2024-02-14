177d16fe097df14e0966163d59e5340de6222a07
Parminder Singh <ps829220@gmail.com>
2024-02-14 11:06:00 +0530

    Add vercel database

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-const connectionString = process.env.POSTGRES_URL || process.env.DATABASE_URL;
+const connectionString = process.env["POSTGRES_URL"] || process.env["DATABASE_URL"];
 const db = pgp(connectionString);
 
 module.exports = {
